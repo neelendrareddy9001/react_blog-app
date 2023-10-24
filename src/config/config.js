482aa32a6fc7d54aefb4892572ab9b24b112a22d
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-10-24 22:12:45 +0200

    updated appwrite database  env file

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+const conf = {
+  appwriteUel: String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteProjectId: String(import.meta.env.VITE_PROJECT_ID),
+  appwriteDatabaseId: String(import.meta.env.VITE_DATABASE_ID),
+  appwriteCollectoinId: String(import.meta.env.VITE_COLLECTION_ID),
+  appwriteBucketId: String(import.meta.env.VITE_BUCKET_ID),
+};
+
+export default conf;
