8ef0d89e21ed75802a87aa8122d7ad2c54692e1a
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-10-25 12:50:21 +0200

    set login and logout actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { logOut, login } from "./store/authSlice";
 
 import "./App.css";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loding, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logOut());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
   return (
     <>
       <h1>Hello World!</h1>
