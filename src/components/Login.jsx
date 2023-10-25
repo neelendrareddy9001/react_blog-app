ffff36f278ab998283f39df6aeb74830322237ad
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-10-25 20:37:21 +0200

    Loginn component with react-hook-form

---
+++ src/components/Login.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { login as authLogin } from "../store/authSlice";
+import { Button, Input, Logo } from "./index";
+import { useDispatch } from "react-redux";
+import { set, useForm } from "react-hook-form";
+import authService from "../appwrite/auth";
+
+function Login() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
+  const [error, setError] = useState("");
+
+  const login = async (data) => {
+    setError("");
+    try {
+      const session = await authService.login(data);
+      if (session) {
+        const userData = await authService.getCurrentUser();
+        if (userData) dispatch(authLogin(userData));
+        navigate("/");
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center w-full">
+      <div
+        className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
+      >
+        <h2 className="text-center text-2xl font-bold leading-tight">
+          Sign in to your accout
+        </h2>
+        <p className="mt-w text-center text-base text-black/60">
+          Don&apos;t have any account?&nbsp;
+          <Link
+            to="/signup"
+            className="font-medium text-primary-all duration-200 hover:underline"
+          >
+            Sign up
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
