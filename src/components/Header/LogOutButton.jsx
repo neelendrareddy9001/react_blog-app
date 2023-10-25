1350cd0f086429563b19244e1934b0354369dd40
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-10-25 19:08:50 +0200

    created input and logout button & header component ui styles

--- src/components/Header/LogOutButton.jsx
+++ src/components/Header/LogOutButton.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import authService from "../../appwrite/auth";
+import { logOut } from "../../store/authSlice";
 
 const LogOutButton = () => {
-  return <div>LogOutButton</div>;
+  const dispatch = useDispatch();
+  const logoutHandler = () => {
+    authService.logout().then(() => {
+      dispatch(logOut());
+    });
+  };
+  return (
+    <button className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full">
+      Logout
+    </button>
+  );
 };
 
 export default LogOutButton;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogOutButton from "./Header/LogOutButton";
 
-export { Header, Footer };
+export { Header, Footer, Container, Logo, LogOutButton };
